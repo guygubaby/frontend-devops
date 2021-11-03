0e5adf79323f7a5b35582690ada78d4bde0700ee
bryce loskie <bryce@bryces-MacBook-Pro.local>
2021-11-03 19:23:45 +0800

    chore: fix some issues and bump up v1.0.8

--- index.mjs
+++ index.mjs
@@ -1,44 +1,46 @@
 import fs from 'fs'
-import path, { dirname } from 'path'
+import path from 'path'
 import { fileURLToPath } from 'url'
 import appRoot from 'app-root-path'
 
 const tryMkdirSync = (dir) => {
   if (!fs.existsSync(dir))
-    fs.mkdirSync(dir)
+    fs.mkdirSync(dir, { recursive: true })
 }
 
-const copy = (src, dest) => {
-  const stat = fs.statSync(src)
-  if (stat.isDirectory())
-    // eslint-disable-next-line no-use-before-define
-    copyDir(src, dest)
-  else
-    fs.copyFileSync(src, dest)
+const copyFile = (src, dest) => {
+  const existed = fs.existsSync(dest)
+  if(existed) return
+  fs.copyFileSync(src, dest)
 }
 
 const copyDir = (srcDir, destDir) => {
-  fs.mkdirSync(destDir, { recursive: true })
+  tryMkdirSync(destDir)
   for (const file of fs.readdirSync(srcDir)) {
     const srcFile = path.resolve(srcDir, file)
     const destFile = path.resolve(destDir, file)
-    copy(srcFile, destFile)
+    copyFile(srcFile, destFile)
   }
 }
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const bootstrap = () => {
   const appPath = appRoot.path
+
+  const appname = appPath.split('/').pop()
+  if(appname==='frontend-devops') return
 
   const dockerignorePath = path.resolve(__dirname, 'dockerignore')
   const rawPath = path.resolve(__dirname, './raw')
   
   const targetPath = path.resolve(appPath, 'devops')
+  const dockerignoreTargetPath = path.resolve(appPath, '.dockerignore')
 
   tryMkdirSync(targetPath)
+
   copyDir(rawPath, targetPath)
-  copy(dockerignorePath, path.resolve(appPath, '.dockerignore'))
+  copyFile(dockerignorePath, dockerignoreTargetPath)
 }
 
 try {
